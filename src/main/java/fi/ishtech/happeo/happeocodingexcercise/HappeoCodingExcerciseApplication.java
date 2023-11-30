58bdb717ff562cae915c6da3da6f1b65186fe372
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-11-30 18:50:49 +0200

    spring start

---
+++ src/main/java/fi/ishtech/happeo/happeocodingexcercise/HappeoCodingExcerciseApplication.java
@@ -0,0 +1,13 @@
+package fi.ishtech.happeo.happeocodingexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappeoCodingExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappeoCodingExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/ishtech/happeo/happeocodingexcercise/HappeoCodingExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.ishtech.happeo.happeocodingexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappeoCodingExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
