8945083b90ecb6b34d499b2f695bdb69e2d89353
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-01 08:18:13 +0200

    tables ddl

---
+++ src/main/resources/db/migration/V02__create_table_user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE t_user (
+  id               BIGSERIAL     NOT NULL,
+  email            VARCHAR(255)  NOT NULL UNIQUE,
+  password_hash    VARCHAR(255)      NULL,
+  first_name       VARCHAR(255)  NOT NULL,
+  last_name        VARCHAR(255)  NOT NULL,
+  organisation_id  BIGINT        NOT NULL,
+  external_id      BIGINT            NULL,
+  is_active        BOOLEAN       NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_user_org_id  FOREIGN KEY (organisation_id) REFERENCES t_organisation(id)
+);
