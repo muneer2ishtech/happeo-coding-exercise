eaa96f285c1c696b1a2968c578736cb6a05bb74d
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-01 16:36:14 +0200

    fix exception caused by getWriter() has already been called for this response

--- src/main/java/fi/ishtech/happeo/codingexercise/security/AuthenticationFilter.java
+++ src/main/java/fi/ishtech/happeo/codingexercise/security/AuthenticationFilter.java
@@ -1,9 +1,7 @@
 package fi.ishtech.happeo.codingexercise.security;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -13,30 +11,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * 
  */
 public class AuthenticationFilter extends GenericFilterBean {
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-      throws IOException, ServletException {
-        try {
-            Authentication authentication = AuthenticationService.getAuthentication((HttpServletRequest) request);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        } catch (Exception exp) {
-            HttpServletResponse httpResponse = (HttpServletResponse) response;
-            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            PrintWriter writer = httpResponse.getWriter();
-            writer.print(exp.getMessage());
-            writer.flush();
-            writer.close();
-        }
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		try {
+			Authentication authentication = AuthenticationService.getAuthentication((HttpServletRequest) request);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		} catch (Exception e) {
+			logger.error("Cannot set user authentication: {}", e);
+		}
 
-        filterChain.doFilter(request, response);
-    }
+		filterChain.doFilter(request, response);
+	}
 
 }
