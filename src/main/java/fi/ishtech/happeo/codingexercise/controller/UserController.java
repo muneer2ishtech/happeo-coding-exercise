767ef2e45419637537adc6e1e2e2a3764197c7bb
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-01 11:38:07 +0200

    org id in user create

--- src/main/java/fi/ishtech/happeo/codingexercise/controller/UserController.java
+++ src/main/java/fi/ishtech/happeo/codingexercise/controller/UserController.java
@@ -47,7 +47,7 @@ public class UserController {
 				provisionerId, user.getId());
 		// TODO: validate token based on organisationId and provisionerId
 
-		UserProvisioningResponse userProvisioningResponse = userService.create(user);
+		UserProvisioningResponse userProvisioningResponse = userService.create(organisationId, user);
 
 		return ResponseEntity.ok(userProvisioningResponse);
 	}
