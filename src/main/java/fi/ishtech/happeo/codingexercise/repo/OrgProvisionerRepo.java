bcf7ebb347fd5aaa0a67273a65acb556dd61c91c
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-01 11:45:16 +0200

    repo for OrgProvisioner

---
+++ src/main/java/fi/ishtech/happeo/codingexercise/repo/OrgProvisionerRepo.java
@@ -0,0 +1,13 @@
+package fi.ishtech.happeo.codingexercise.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fi.ishtech.happeo.codingexercise.entity.OrgProvisioner;
+
+/**
+ *
+ * @author Muneer Ahmed Syed
+ */
+public interface OrgProvisionerRepo extends JpaRepository<OrgProvisioner, Long> {
+
+}
