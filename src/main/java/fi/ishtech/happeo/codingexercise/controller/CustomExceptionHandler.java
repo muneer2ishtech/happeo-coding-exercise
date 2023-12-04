8518d71e32548e3306f7653120e506bbd2eb02f5
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-04 14:46:23 +0200

    organize custom exception to proper package

--- src/main/java/fi/ishtech/happeo/codingexercise/controller/CustomExceptionHandler.java
+++ src/main/java/fi/ishtech/happeo/codingexercise/controller/CustomExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import fi.ishtech.happeo.codingexercise.MissingOrganisationProvisionerException;
+import fi.ishtech.happeo.codingexercise.exception.MissingOrganisationProvisionerException;
 import fi.ishtech.happeo.codingexercise.payload.response.CustomErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 
