c75811d2f600c3564fe146ff0748349a2d86506d
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-06 18:38:29 +0200

    fix mapping policy of orgprovisioner mapper

--- src/main/java/fi/ishtech/happeo/codingexercise/mapper/OrgProvisionerMapper.java
+++ src/main/java/fi/ishtech/happeo/codingexercise/mapper/OrgProvisionerMapper.java
@@ -2,6 +2,7 @@ package fi.ishtech.happeo.codingexercise.mapper;
 
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import fi.ishtech.happeo.codingexercise.entity.OrgProvisioner;
 import fi.ishtech.happeo.codingexercise.payload.response.OrgProvisionerResponse;
@@ -14,7 +15,7 @@ import fi.ishtech.happeo.codingexercise.payload.response.OrgProvisionerResponse;
 @Mapper(componentModel = "spring", uses = { OrganisationMapper.class, ProvisionerMapper.class })
 public interface OrgProvisionerMapper {
 
-	@BeanMapping(ignoreByDefault = true)
+	@BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 	OrgProvisionerResponse toResponse(OrgProvisioner orgProvisioner);
 
 }
