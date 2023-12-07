89d900b3de1232f12402a25f03a56452bb330a83
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-07 07:26:40 +0200

    return simple unathorized for 401

--- src/main/java/fi/ishtech/happeo/codingexercise/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/fi/ishtech/happeo/codingexercise/security/jwt/JwtAuthenticationEntryPoint.java
@@ -19,8 +19,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException {
 
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-				authException != null ? authException.getMessage() : "Unauthorized");
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
 	}
 
 }
