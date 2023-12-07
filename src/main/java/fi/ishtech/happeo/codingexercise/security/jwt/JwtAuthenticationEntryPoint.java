fcfb969713e9ad2779df86b8abff5cda1b179a48
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-12-07 05:47:56 +0200

    jwt entry point to handle authentication exception as 401 - unathorized

---
+++ src/main/java/fi/ishtech/happeo/codingexercise/security/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package fi.ishtech.happeo.codingexercise.security.jwt;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final long serialVersionUID = -8678995890638658913L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+				authException != null ? authException.getMessage() : "Unauthorized");
+	}
+
+}
